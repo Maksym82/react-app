29fe69cf08efaa49b7a6c7fda053ac8c1603cfc4
Maksym <112901851+Maksym82@users.noreply.github.com>
2025-05-11 20:50:07 +0300

    Update index.html

--- src/index.js
+++ src/index.js
@@ -3,11 +3,10 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-const elem = <h2>Hello world!</h2>;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
- elem
+ <App/>
 );
 
 
